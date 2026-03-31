220e2427a02823f400a53c6cf9eeb5cad1b639c1
Salman5371 <salmanfarshi.sdk@gmail.com>
2026-03-31 18:34:25 +0600

    Updated Footer

--- gobangla/app/Component/Footer.tsx
+++ gobangla/app/Component/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-white text-gray-800 p-10">
+    <footer className="footer sm:footer-horizontal bg-white text-gray-800 px-4 py-2">
       <aside>
         <svg
           width={50}
@@ -17,15 +17,20 @@ const Footer: React.FC = () => {
           <path d="M22.672 15.226l-2.432...-4.829z" />
         </svg>
 
-        <p>
+        <p className="mt-2 text-2xl text-semibold text-blue-900">
           GoBangla Group.
-          <br />
-          Providing reliable tech since 1992
         </p>
+
+        <p className="mt-2 text-md text-blue-900">
+           2/1 Lalmatia, Dhaka-1207, Bangladesh.
+        </p>
+
+        
       </aside>
 
       <nav>
-        <h6 className="footer-title">Social</h6>
+        <h6 className="mt-3 text-md text-black text-center">
+           Copyright © 2026 • All Rights Reserved</h6>
         <div className="grid grid-flow-col gap-4">
           <a>
             <svg width={24} height={24} viewBox="0 0 24 24" className="fill-current">
