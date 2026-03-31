d1312315b49838c4860791dd834ad7256934e44e
MOHTASIM-UR-RAHMAN <novarahman2003@gmail.com>
2026-03-31 17:34:36 +0600

    updatedNavbar

--- gobangla/app/Component/Navbar.tsx
+++ gobangla/app/Component/Navbar.tsx
@@ -2,24 +2,30 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import AboutPage from "../about/page";
 
 const Navbar: React.FC = () => {
   return (
     <div className="w-full navbar bg-white shadow-md px-6 py-4 fixed top-0 left-0 z-50 flex items-center gap-3">
       
-      //logo
-      <Image
+      <Link href="/">
+      <Image 
         src="/Logo.jpg" 
         alt="Logo"
         width={80}
         height={80}
       />
+      </Link>
 
-      <Link href="/">
       <span className="text-2xl font-semibold text-blue-900">
         GoBangla Group
       </span>
-        </Link>
+        <div className="ml-auto flex items-center gap-6">
+          <Link href="/about" className="text-lg font-medium text-gray-700 hover:text-blue-900">
+            About
+          </Link>
+        </div>
+
     </div>
   );
 };
