74a2848d7bb61b5f1b29ad6dff7804a93f712fec
MOHTASIM-UR-RAHMAN <novarahman2003@gmail.com>
2026-03-31 17:41:45 +0600

    fix

--- gobangla/app/Component/Navbar.tsx
+++ gobangla/app/Component/Navbar.tsx
@@ -8,9 +8,9 @@ const Navbar: React.FC = () => {
   return (
     <div className="w-full navbar bg-white shadow-md px-6 py-4 fixed top-0 left-0 z-50 flex items-center gap-3">
       
-      //logo
-      <Image
-        src="/Logo.jpeg" 
+      <Link href="/">
+      <Image 
+        src="/Logo.jpg" 
         alt="Logo"
         width={80}
         height={80}
@@ -20,12 +20,7 @@ const Navbar: React.FC = () => {
       <span className="text-2xl font-semibold text-blue-900">
         GoBangla Group
       </span>
-        <div className="ml-auto flex items-center gap-6">
-          <Link href="/about" className="text-lg font-medium text-gray-700 hover:text-blue-900">
-            About
-          </Link>
-        </div>
-
+      
     </div>
   );
 };
