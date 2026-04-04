8a3cf82932b2b556ccb4b6cc171822e1cbabf0f8
MOHTASIM-UR-RAHMAN <novarahman2003@gmail.com>
2026-04-04 16:24:58 +0600

    updateServiceSlider

--- gobangla/app/Component/ServiceSlider.tsx
+++ gobangla/app/Component/ServiceSlider.tsx
@@ -2,32 +2,33 @@
 import { useEffect, useState } from "react";
 
 export default function ServiceSlider() {
-  const images = ["/Logo.jpg", "/Logo.png", "/Solutions.jpg", "/Academy.jpg", "/ToursLogo.jpg", 
-                    "/ShikbeLogo.jpg", "/StayinLogo.jpg"];
+  const images = [ "/Logo.png", "/Logo.jpg", "/Solutions.jpg", "/Academy.jpg", "/ToursLogo.jpg",
+    "/ShikbeLogo.jpg", "/StayinLogo.jpg"];
+
   const [index, setIndex] = useState(0);
-  const [animate, setAnimate] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setAnimate(false);
-
-      setTimeout(() => {
-        setIndex((prev) => (prev + 1) % images.length);
-        setAnimate(true);
-      }, 1000); // fade out time
-    }, 4000); // change every 4 sec
+      setIndex((prev) => (prev + 1) % images.length);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="w-full h-[400px] overflow-hidden rounded-xl">
-      <img
-        src={images[index]}
-        alt="Slide"
-        className={`w-full h-full object-cover transition-all duration-1000 ease-in-out
-        ${animate ? "opacity-100 scale-100" : "opacity-0 scale-110"}`}
-      />
+      <div
+        className="flex transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${index * 100}%)` }}
+      >
+        {images.map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            className="w-full h-[400px] object-cover flex-shrink-0"
+          />
+        ))}
+      </div>
     </div>
   );
 }
