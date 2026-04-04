74cc72df5ad2bddcaac7f6ad047d810c26f0aa0c
MOHTASIM-UR-RAHMAN <novarahman2003@gmail.com>
2026-04-04 13:58:23 +0600

    navbarUpdate

--- gobangla/app/Component/Navbar.tsx
+++ gobangla/app/Component/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC = () => {
       
       <Link href="/">
       <Image
-        src="/Logo.jpeg" 
+        src="/Logo.png" 
         alt="Logo"
         width={80}
         height={80}
