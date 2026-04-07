ba75e299afcb2f7847f1fdef33448c56251e835a
Salman5371 <salmanfarshi.sdk@gmail.com>
2026-04-07 17:46:21 +0600

    update LinkedIn

--- gobangla/app/about/page.tsx
+++ gobangla/app/about/page.tsx
@@ -220,7 +220,7 @@ export default function AboutPage() {
 
               {/* Card 6 */}
               <div className="card bg-base-200 p-6 text-center shadow-lg h-full flex flex-col justify-between">
-                <Link href="https://github.com/sayedsafi2000" target="_blank">
+                <Link href="https://www.linkedin.com/in/sayed-safi/" target="_blank">
                   <Image
                     src="/Safi.jpeg"
                     alt="Syed Safi"
