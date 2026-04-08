358f995059155295aed520000becd0c06f9d8ccb
MOHTASIM-UR-RAHMAN <novarahman2003@gmail.com>
2026-04-08 12:18:12 +0600

    next updated

--- gobangla/next.config.ts
+++ gobangla/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: './', // 🔥 CRITICAL FIX
 };
 
 export default nextConfig;
